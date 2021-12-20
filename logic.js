1f5259a303bd8b6b7f6635fdc8fcdcefa35467a6
João Pedro Gobain <50881995+Gobain@users.noreply.github.com>
2021-12-20 16:38:18 -0300

    Update logic.js

--- logic.js
+++ logic.js
@@ -16,7 +16,7 @@ function getPrice() {
 function start() {
   
   let yearValue = [0.83, 9.57, 40.59, 2262.50, 584.24, 1475.50, 3041.32,
-  32643.97, 12511.40, 40014.14]; //price of BTC in BRL on 1st day of the year, starting at 2011
+  32643.97, 12511.40, 40014.14, 151000.00]; //price of BTC in BRL on 1st day of the year, starting at 2011
   
   //todayValue = 300000;
   let amount = document.getElementById("amount");
